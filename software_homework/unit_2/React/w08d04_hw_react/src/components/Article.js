553e0e3a420007e0d3812ddb8eaa4e8e23edd694
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2023-12-13 13:56:25 -0500

    react homework

---
+++ software_homework/unit_2/React/w08d04_hw_react/src/components/Article.js
@@ -0,0 +1,29 @@
+function Article(){
+    return(
+        <div>
+        <main>
+<article>
+    <time>11/12/20</time>
+    <h3>On the Street in Brooklyn</h3>
+    <img src="https://ps-wise-course.herokuapp.com/fashionblog/blog-image-1.jpg" alt="girl"/>
+    <p id="first-article">Cray ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis nunc eget lorem dolor sed viverra. Lectus sit amet est placerat. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Diam sollicitudin tempor id eu nisl. Nunc mattis enim ut tellus elementum sagittis vitae et leo. Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Enim lobortis scelerisque fermentum dui. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Nunc sed augue lacus viverra vitae congue. Sociis natoque penatibus et magnis dis parturient. Ac placerat vestibulum lectus mauris ultrices. Nibh mauris cursus mattis molestie a iaculis at. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Eros donec ac odio tempor orci dapibus ultrices. Enim tortor at auctor urna nunc id cursus metus.
+
+  </p>
+    <a class="continues">Continues...</a>
+</article>
+    <hr/>
+<article>
+    <time>11/12/20</time>
+    <h3>Vintage in Vogue</h3>
+    <img src="https://ps-wise-course.herokuapp.com/fashionblog/blog-image-2.jpg" alt="people"/>
+<p id="second-article">Selfies sunt, dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra vel turpis nunc eget lorem dolor sed viverra. Lectus sit amet est placerat. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Diam sollicitudin tempor id eu nisl. Nunc mattis enim ut tellus elementum sagittis vitae et leo. Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Enim lobortis scelerisque fermentum dui. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Nunc sed augue lacus viverra vitae congue. Sociis natoque penatibus et magnis dis parturient. Ac placerat vestibulum lectus mauris ultrices. Nibh mauris cursus mattis molestie a iaculis at. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Eros donec ac odio tempor orci dapibus ultrices. Enim tortor at auctor urna nunc id cursus metus.
+
+</p>
+<a class="continues">Continues...</a>
+</article>
+<hr/>
+    </main>
+    </div>
+    )
+}
+export default Article;
---
+++ software_homework/unit_2/React/w08d04_hw_react/src/components/Footer.js
@@ -0,0 +1,20 @@
+function Footer(){
+    return(
+        <div>
+  <footer>
+        <ul id="footer-nav">
+            <li><a>Home</a></li>
+            <li><a>Women's</a></li>
+            <li><a>Men's</a></li>
+            <li><a>On the Street</a></li>
+            <li><a>The Catwalk</a></li>
+            <li><a>AdWatch</a></li>
+            <li><a>About</a></li>
+            <li><a>Tips</a></li>
+        </ul>
+        <p id="copyright"> 	&copy; 2013 Valet Industries, Inc</p>
+    </footer>
+        </div>
+    )
+}
+export default Footer;
