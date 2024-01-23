6615274dd19894e2df0722a96130c6bfc5e3f6cf
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-01-23 12:33:36 -0500

    Tip calculator express application created

--- software_homework/unit_3/w12d02_lab/server.js
+++ software_homework/unit_3/w12d02_lab/server.js
@@ -13,6 +13,11 @@ app.get('/greeting/:name', (req, res)=>{
     res.send("Hello,"+" " + req.params.name + "! It's so great to see you!");
 })
 
+app.get('/tip/:total/:percent', (req, res)=>{
+    console.log(req.params);
+    res.send("Based on the $"+req.params.total+" total and "+req.params.percent+"% tip, the tip will be $"+req.params.total*(req.params.percent/100))
+})
+
 app.listen(3000, function() {
     console.log("Listening on the port 3000")
 })
