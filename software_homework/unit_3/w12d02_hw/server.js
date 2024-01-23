81fc331414673bf0ca73c43cb345678c7496ad82
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-01-23 14:45:26 -0500

    bolles count

---
+++ software_homework/unit_3/w12d02_hw/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+let bottle_count=5;
+
+app.get('/', (req, res)=>{
+    res.send(`
+    <h1>${bottle_count} Bottles of beer on the wall</h1> 
+    <br/>
+    <a href="/${bottle_count - 1}">Take one down, pass it around</a>
+  `);
+});
+
+
+app.get(`/:number_of_bottles`, (req,res)=>{
+    const {number_of_bottles} = req.params;
+    const bottles_left = parseInt(number_of_bottles);
+    if(bottles_left===0){
+        res.send(`
+      <h1>No more bottles of beer on the wall</h1>
+      <a href="/">Start over</a>
+    `);
+  } else {
+    res.send(`
+      <h1>${bottles_left} Bottles of beer on the wall</h1>
+      <a href="/${bottles_left - 1}">Take one down, pass it around</a>
+    `);
+  }
+});
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000")
+})
