0f102518eea7922756944fe8211ecbe3e51102e7
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-02-05 10:22:31 -0500

    homework

---
+++ unit_2/React/w09d04_hw_star_wars_api/src/components/StarShipCard.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+export default function StarShipCard({starship}) {
+    return (
+        <div className="starship-card">
+            <Link to={`/starships/${starship.name}`}>
+            <h3>{starship?.name}</h3>
+            <p>Model: {starship?.model}</p>
+            <p>Manufacturer: {starship?.manufacturer}</p>
+            </Link>
+           
+        </div>
+    )
+};
---
+++ unit_2/React/w09d04_hw_star_wars_api/src/services/api.js
@@ -0,0 +1,5 @@
+export async function getAllStarships() {
+
+    const res = await fetch(`https://swapi.dev/api/starships/`);
+    return await res.json();
+  };
