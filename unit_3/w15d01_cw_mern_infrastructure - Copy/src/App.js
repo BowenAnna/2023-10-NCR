fe90da0fcfb4dd951d903bfbb208e57631a0db6b
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-02-12 12:53:42 -0500

    created auth page and order pages

---
+++ unit_3/w15d01_cw_mern_infrastructure - Copy/src/App.js
@@ -0,0 +1,31 @@
+//imports
+import './App.css';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+//pages
+import AuthPage from './pages/Auth';
+import NewOrderPage from './pages/NewOrder';
+import OrderHistoryPage from './pages/OrderHistory';
+//components
+import NavBar from "./components/Nav"
+
+
+export default function App() {
+  const[user, setUser]=useState({})
+  return (
+    <main className="App">
+      { user ? 
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+          </Routes>
+        </>
+        :
+        <AuthPage />
+      }
+    </main>
+  );
+}
+
